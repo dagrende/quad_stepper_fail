a9b1278e30779518e50fd0dd212f928c541dcd84
dagrende <dag.rende@find-out.se>
2015-12-28 19:22:39 +0100

    removed debouncing, added dividing by m/d, added dir,step pulsing

--- quad_stepper.ino
+++ quad_stepper.ino
@@ -1,55 +1,74 @@
-#include <Bounce2.h>
+// quadrature stepper
 
+#define dirOut 0
+#define stepOut 1
 #define q0Sensor 2
 #define q1Sensor 3
 
 int state = 0;
 // index by ssqq where ss=state and qq=q1q0
+// value is bfss where d=1 if backward, f=1 if forward, ss=new state
 byte nextState[] = {
-  0b00,  //0000 same
-  0b01,  //0001 forw
-  0b11,  //0010 back
-  0b01,  //0011 error forw
+  0b0000,  //0000 same
+  0b0101,  //0001 forw
+  0b1011,  //0010 back
+  0b0101,  //0011 error forw
   
-  0b00,  //0100 back
-  0b01,  //0101 same
-  0b10,  //0110 error forw
-  0b10,  //0111 forw
+  0b1000,  //0100 back
+  0b0001,  //0101 same
+  0b0110,  //0110 error forw
+  0b0110,  //0111 forw
   
-  0b11,  //1000 error forw
-  0b01,  //1001 back
-  0b11,  //1010 forw
-  0b10,  //1011 same
+  0b0111,  //1000 error forw
+  0b1001,  //1001 back
+  0b0111,  //1010 forw
+  0b0010,  //1011 same
   
-  0b00,  //1100 forw
-  0b00,  //1101 error forw
-  0b11,  //1110 same
-  0b10  //1111 back
+  0b0100,  //1100 forw
+  0b0100,  //1101 error forw
+  0b0011,  //1110 same
+  0b1010  //1111 back
 };
   
-
-Bounce q0SensorDebouncer = Bounce();
-Bounce q1SensorDebouncer = Bounce();
+long int m = 1;
+long int d = 8;
+long int sum = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   
   pinMode(q0Sensor, INPUT_PULLUP);
-  q0SensorDebouncer.attach(q0Sensor);
-  q0SensorDebouncer.interval(10);
-  
   pinMode(q1Sensor, INPUT_PULLUP);
-  q1SensorDebouncer.attach(q1Sensor);
-  q1SensorDebouncer.interval(10);
+  pinMode(dirOut, OUTPUT);
+  pinMode(stepOut, OUTPUT);
 }
 
 void loop() {
-  if (q0SensorDebouncer.update() || q1SensorDebouncer.update()) {
-    int q0 = 1 & !q0SensorDebouncer.read();
-    int q1 = 1 & !q1SensorDebouncer.read();
-    int i = (state & 3) << 2 | (q1 << 1) | q0;
-    state = nextState[i];
-    Serial.println(state);
-  }
+    // handle m,d input here
+  
+    byte newState = nextState[(state & 3) << 2 | (~PIND & 3)];
+    state = newState & 0b11;
+    byte forward = (newState & 0b0100) != 0;
+    byte backward = (newState & 0b1000) != 0;
+    digitalWrite(dirOut, forward);
+    if (forward) {
+      // quad back
+      sum += m;
+      if (sum >= d) {
+        digitalWrite(stepOut, 1);
+        sum -= d;
+        digitalWrite(stepOut, 0);
+      }
+      Serial.println(sum);
+    } else if (backward) {
+      // quad back
+      sum -= m;
+      if (sum < 0) {
+        digitalWrite(stepOut, 1);
+        sum += d;
+        digitalWrite(stepOut, 0);
+      }
+      Serial.println(sum);
+    }
 }
